8167ebd52f0f9aaf00d48881e754f8dcafed9de9
Alex Young <alexyoung0119@gmail.com>
2025-11-25 21:39:01 -0600

    Initial Commit

---
+++ tte/tteApp.swift
@@ -0,0 +1,17 @@
+//
+//  tteApp.swift
+//  tte
+//
+//  Created by Alex Young on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct tteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
